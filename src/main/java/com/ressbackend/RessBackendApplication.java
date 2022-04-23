9ee4b1defc7a66cc9ddcaf38ae1da3fff026a356
amil m <you@example.com>
2022-04-23 15:41:39 +0200

    My services and controllers, they implement possibility to display all restaurants, search by ID and location, search only clubs, delete, post

---
+++ src/main/java/com/ressbackend/RessBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ressbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RessBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RessBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ressbackend/controllers/RestaurantController.java
@@ -0,0 +1,54 @@
+package com.ressbackend.controllers;
+import com.ressbackend.models.Restaurant;
+import com.ressbackend.services.RestaurantService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/restaurants")
+public class RestaurantController {
+
+    private final RestaurantService restaurantService;
+
+    public RestaurantController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
+
+    @GetMapping()
+    public List<Restaurant> getRestaurants() {
+        return this.restaurantService.getRestaurants();
+    }
+
+    @GetMapping("/{id}")
+    public Restaurant getRestaurant(@PathVariable long id) {
+        return this.restaurantService.getById(id);
+    }
+
+    @GetMapping("/clubs")
+    public List<Restaurant> getClubs() {
+        return this.restaurantService.getOnlyClubs();
+    }
+
+    @GetMapping("/location/{location}")
+    public List<Restaurant> getByLocation(@PathVariable String location) {
+        return this.restaurantService.getByLocation(location);
+    }
+
+    @PostMapping
+    public Restaurant createRestaurant(@RequestBody Restaurant restaurant){
+        return this.restaurantService.createRestaurant(restaurant);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteRestaurant (@PathVariable long id) {
+        return this.restaurantService.deleteById(id);
+    }
+
+
+}
+
+
+
+
+
