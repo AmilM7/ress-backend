aa1d0c968505a820a9e1cf033ec0c997caa0be90
IrfanParic <irfan.paric@stu.ssst.edu.ba>
2022-05-03 16:27:10 +0200

    Added: Reservation Contoller, Reservation model, Reservation repository and Reservation service

--- src/main/java/com/ressbackend/models/Reservation.java
+++ src/main/java/com/ressbackend/models/Reservation.java
@@ -1,15 +1,38 @@
 package com.ressbackend.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="reservation")
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class Reservation extends User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "number_of_guests")
     private long numberOfGuests;
+
+    @Column(name = "time")
     private long time;
+
+    @Column(name = "table_id")
     private long tableId;
+
+    @Column(name = "day")
     private String day;
+
+    @Column(name = "user_first_name")
     private String userFirstName = User.firstName;
+
+    @Column(name = "user_last_name")
+    private String userLastName = User.lastName;
 }
---
+++ src/main/java/com/ressbackend/repositories/ReservationRepository.java
@@ -0,0 +1,7 @@
+package com.ressbackend.repositories;
+
+import com.ressbackend.models.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+}
