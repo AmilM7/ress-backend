a0ecae0ff39d6e6b36205cff03118d70ef24009f
amil m <you@example.com>
2022-06-01 23:47:50 +0200

    Added services for reservation that will enable to search reservations with user and restaurant foreign key

--- src/main/java/com/ressbackend/controllers/ReservationController.java
+++ src/main/java/com/ressbackend/controllers/ReservationController.java
@@ -13,28 +13,38 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
-    public ReservationController(ReservationService reservationService){
+    public ReservationController(ReservationService reservationService) {
         this.reservationService = reservationService;
     }
 
     @GetMapping
-    public List<Reservation> getReservation(){
+    public List<Reservation> getReservation() {
         return reservationService.getReservation();
     }
 
     @GetMapping("/{id}")
-    public Reservation getReservation(@PathVariable long id){
+    public Reservation getReservation(@PathVariable long id) {
         return this.reservationService.getById(id);
     }
 
     @GetMapping("/getSpecificUserReservationbyTableId/{id}")
-    public List<Reservation> getSpecificReservation(@PathVariable long id)  {
+    public List<Reservation> getSpecificReservation(@PathVariable long id) {
         return this.reservationService.getReservationByTableId(id);
     }
 
-    @RequestMapping(value="/day/{day}", method = RequestMethod.GET)
-    public List<Reservation> getReservationByDay(@PathVariable String day){
+    @RequestMapping(value = "/day/{day}", method = RequestMethod.GET)
+    public List<Reservation> getReservationByDay(@PathVariable String day) {
         return this.reservationService.getByDay(day);
+    }
+
+    @GetMapping("/userID/{id}")
+    public List<Reservation> getReservationByUserID(@PathVariable long id) {
+        return reservationService.getReservationByUser(id);
+    }
+
+    @GetMapping("/restaurantID/{id}")
+    public List<Reservation> getReservationByRestaurantID(@PathVariable long id) {
+        return reservationService.getReservationByRestaurant(id);
     }
 
     @PostMapping
@@ -55,7 +65,7 @@ public class ReservationController {
     }
 
     @DeleteMapping("/{id}")
-    public Reservation deleteReservation(@PathVariable long id){
+    public Reservation deleteReservation(@PathVariable long id) {
         reservationService.deleteReservation(id);
         return null;
     }
